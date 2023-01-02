da3f72bcaab72e9f93b7563df49097ec98d10429
Mudit <muditert34@gmail.com>
2023-01-02 21:58:12 +0530

    Inital commit

---
+++ src/components/Employee.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line
+
+const Employees = [
+    {
+        id: '1',
+        Name: 'Mudit Jain',
+        Age: '18'
+    },
+    {
+        id: '2',
+        Name: 'Aditi Garg',
+        Age: '18'
+    },
+    {
+        id: '3',
+        Name: 'Ankur Garg',
+        Age: '19'
+    }
+]
+
+export default Employees;
---
+++ src/components/Home.js
@@ -0,0 +1,80 @@
+// eslint-disable-next-line
+
+import React, { Fragment } from "react";
+import { Button, Table } from 'react-bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Employees from "./Employee";
+
+import { Link, useNavigate } from "react-router-dom";
+
+function Home() {
+
+    let history = useNavigate();
+
+    const handleDelete = (id) => {
+        var index = Employees.map(function (e) {
+            return e.id;
+        }).indexOf(id);
+
+        Employees.splice(index, 1);
+
+        history('/');
+
+
+    }
+
+    const handleEdit = (id, name, age) => {
+        localStorage.setItem('Name', name);
+        localStorage.setItem('Age', age);
+        localStorage.setItem('Id', id);
+    }
+
+    return (
+        <Fragment>
+            <div style={{ margin: "10rem" }}>
+                <Table striped bordered hover size="sm">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Age</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            Employees && Employees.length > 0
+                                ?
+                                Employees.map((item) => {
+                                    return (
+                                        <tr>
+                                            <td>{item.Name}</td>
+                                            <td>{item.Age}</td>
+                                            <td>
+                                                <Link to={`/edit`}>
+                                                    <Button onClick={() => handleEdit(item.id, item.name, item.age)}>Edit</Button>
+                                                </Link>
+                                                &nbsp;
+                                                <Button onClick={() => handleDelete(item.id)}>Delete</Button>
+                                            </td>
+
+                                        </tr>
+                                    )
+                                })
+                                :
+                                "No data available"
+                        }
+                    </tbody>
+                </Table>
+                <Link to="/create">
+                    <Button>
+                        Create
+                    </Button>
+                </Link>
+            </div>
+        </Fragment>
+    )
+}
+
+export default Home
